90a1370834163f5466afa57b87902cb7b2d6bb1a
Gravity06 <leojf9818@gmail.com>
2025-08-19 21:52:30 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Aula de método</title>
+</head>
+<body>
+  <form action="desafio1.php" method="get">
+    Nome: <input type="text" name="cxnome">
+    <input type="submit" value="Enviar">
+  </form>
+</body>
+</html>
